66b384d58f09e10506904a19d6a0cd8d05fac274
Olha Romanova <larklarky@gmail.com>
2019-01-23 20:20:47 +0100

    implementation of search

--- flaskblog/main/routes.py
+++ flaskblog/main/routes.py
@@ -7,8 +7,9 @@ main = Blueprint('main', __name__)
 @main.route("/")
 @main.route("/home")
 def home():
+    query = request.args.get('query', '')
     page = request.args.get('page', 1, type=int)
-    posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=5)
+    posts = Post.query.filter(Post.title.contains(query)).order_by(Post.date_posted.desc()).paginate(page=page, per_page=5)
     return render_template('home.html', posts=posts)
 
 
