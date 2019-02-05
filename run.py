fee243ecda58dc9c821fc700db7fec110a49cfc2
Olha Romanova <larklarky@gmail.com>
2019-02-05 22:04:21 +0100

    added function that creates admin users

--- run.py
+++ run.py
@@ -1,6 +1,17 @@
-from flaskblog import create_app
+from flaskblog import create_app, db
+from flaskblog.models import User
+import click
 
 app = create_app()
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+@app.cli.command()
+@click.argument('username')
+@click.argument('email')
+@click.argument('password')
+def create_user(username, email, password):
+    admin = User(username=username, email=email, password=password, is_admin=True)
+    db.session.add(admin)
+    db.session.commit()
